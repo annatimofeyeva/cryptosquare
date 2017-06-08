4a5dbe4f9c06498ef4bcf3c6cdee49377d8af15e
Anna Timofeyeva and Esvethlynna Pangelinan <esvethlynna@>
2017-06-08 16:00:57 -0700

    determine number of columns and rows and convert the string to an array of letters

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,21 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     var inputSentence = $("input#sentence").val().toLowerCase();
-    alert(inputSentence.replace(/[^A-Za-z0-9_]/g,""));
-    var numberOfLetters = inputSentence.length;
+    var newSentence = inputSentence.replace(/[^A-Za-z0-9_]/g,"");
+    var newSentenceArray = newSentence.split("");
+    alert(newSentenceArray);
+    var numberOfLetters = newSentenceArray.length;
+
 
     //alert(inputSentence.length);
     //take the square root of the number of letters to get number of columns
     var numberOfColumns = Math.sqrt(numberOfLetters);
-    alert(numberOfColumns);
+    var numOfColumnsRounded = Math.round(numberOfColumns);
+    alert(numOfColumnsRounded);
+    //$('tr').append('a');
 
     //determine number of rows:
-    var numberOfRows = Math.round(numberOfLetters/numberOfColumns);
+    var numberOfRows = Math.round(numberOfLetters/numOfColumnsRounded);
     alert(numberOfRows);
   });
 });
