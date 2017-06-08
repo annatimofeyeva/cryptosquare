dc755cbe9007b3f311ad5b8268a9a7e75f363a1e
Anna Timofeyeva and Esvethlynna Pangelinan <esvethlynna@>
2017-06-08 16:31:59 -0700

    recalculate the number of Rows

--- js/scripts.js
+++ js/scripts.js
@@ -11,12 +11,12 @@ $(document).ready(function() {
     //alert(inputSentence.length);
     //take the square root of the number of letters to get number of columns
     var numberOfColumns = Math.sqrt(numberOfLetters);
-    var numOfColumnsRounded = Math.round(numberOfColumns);
+    var numOfColumnsRounded = Math.floor(numberOfColumns);
     alert(numOfColumnsRounded);
     //$('tr').append('a');
 
     //determine number of rows:
-    var numberOfRows = Math.round(numberOfLetters/numOfColumnsRounded);
+    var numberOfRows = Math.ceil(numberOfLetters/numOfColumnsRounded);
     alert(numberOfRows);
   });
 });
